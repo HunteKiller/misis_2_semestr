92101d36c02b1451c775b8f524ecba5db7b2b1d8
HunteKiller <132778432+HunteKiller@users.noreply.github.com>
2025-02-15 18:26:23 +0300

    Update utilities.cpp

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+void i_plus(int a, int b){
+    std::cout << a+b;
+}
+
+void minus(int a, int b){
+    std::cout << a-b;
+}
+
+void mul(int a, int b){
+    std::cout << a*b;
+}
+
+
+void div_d(int a, int b){
+    if(b != 0){
+        std::cout << a/b;
+    }
+    else{
+        std::cout<< "Not Exist";
+    }
+    
+}
