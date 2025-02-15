80de73e6960bca83992b4d7e29d246191e9fdc71
HunteKiller <132778432+HunteKiller@users.noreply.github.com>
2025-02-15 18:25:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "utilits.cpp"
 
+int main(){
+    std::cout << "Это плюс!";
+    i_plus(10, 1999999);
+
+    std::cout << "Это минус!";
+    minus(10, 1999999);
+
+    std::cout << "Это умножение!";
+    mul(10, 1999999);
+
+    std::cout << "Это деление!";
+    div_d(10, 1999999);
+}
